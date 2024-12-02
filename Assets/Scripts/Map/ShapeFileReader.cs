e6b3807dd90252c27c3067f910506e7fde6e43ee
SuroBuro <dasbiswaji104@gmail.com>
2024-12-03 01:57:11 +0530

    First Iteration of the project with the Shapefile Reader

---
+++ Assets/Scripts/Map/ShapeFileReader.cs
@@ -0,0 +1,93 @@
+//This code had been heavily influenced by ChatGPT. Thus, a more robust implementation of the code is being awaited
+
+using NetTopologySuite.IO.ShapeFile.Extended;
+using NetTopologySuite.Features;
+using NetTopologySuite.Geometries;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using NetTopologySuite.IO;
+
+public class ShapeFileReader : MonoBehaviour
+{
+    [Tooltip("Path to shapefile (.shp)")]
+    public string ShapeFilePath;
+
+    private List<Vector3[]> lines;          //Stores lines for visualisation
+    private List<Vector3[]> polygons;       //Stores vertices of Polygons
+    private List<Vector3> points;           //Stores point features
+    void Start()
+    {
+        lines = new List<Vector3[]>();
+        polygons = new List<Vector3[]>();
+        points = new List<Vector3>();
+
+        ReadShapefile();
+
+        DrawData();
+    }  
+    void ReadShapefile()
+     { 
+        using (ShapefileDataReader reader = new (ShapeFilePath, GeometryFactory.Default))
+        {
+            while (reader.Read())
+            {
+                Geometry geometry = reader.Geometry; ;
+
+                //For Point or Polygon conversion
+                if (geometry is Point point)
+                {
+                    points.Add(ToUnityCoordinates(point));
+                }
+                else if (geometry is Polygon polygon)
+                {
+                    
+                    polygons.Add(ConvertToUnityCoordinates(polygon.ExteriorRing.Coordinates));
+                    
+                }
+                else if(geometry is LineString lineString)
+                {
+                    lines.Add(ConvertToUnityCoordinates(lineString.Coordinates));
+                }
+            }
+        }
+    }
+
+     Vector3 ToUnityCoordinates(Point point)
+    {
+        return new Vector3((float)point.X, 0, (float)point.Y);
+    }
+
+    Vector3[] ConvertToUnityCoordinates(Coordinate[] coordinates)
+    {
+        Vector3[] unityCoordinates = new Vector3[coordinates.Length];
+        for (int i = 0; i < coordinates.Length; i++)
+        {
+            unityCoordinates[i] = new Vector3((float)coordinates[i].X, 0,(float)coordinates[i].Y);
+
+        }
+        return unityCoordinates;
+    }
+
+    void DrawData()
+    {
+        foreach (var point in points)
+        {
+            Debug.DrawRay(point, Vector3.up * 10, Color.green, 100f);
+        }
+        foreach (var polygon in polygons)
+        {     
+            for (int i = 0;i < polygon.Length;i++) 
+              {
+                Debug.DrawLine(polygon[i], polygon[i + 1], Color.blue, 100f);
+              }
+        }
+        foreach (var line  in lines)
+        {
+            for (int i = 0; i< line.Length;i++)
+            {
+                Debug.DrawLine(line[i], line[i + 1], Color.red, 100f);
+            }
+        }
+    }
+}
